90a287fabd9710868e2e3c046592c77388bbc8a9
manojdongiri <manojdongiri@gmail.com>
2026-04-26 14:58:17 +0530

    fix app error

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+# ROUTES IMPORT
 from app.routes.product_routes import router as product_router
 from app.routes.order_routes import router as order_router
 from app.routes.auth_routes import router as auth_router
-app.include_router(auth_router)
+
+# ✅ CREATE APP FIRST
 app = FastAPI()
 
-# CORS
+# ✅ CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -15,16 +18,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# ROUTES
+# ✅ INCLUDE ROUTES AFTER APP CREATED
 app.include_router(product_router)
 app.include_router(order_router)
+app.include_router(auth_router)
 
-# ROOT (IMPORTANT)
+# ROOT
 @app.get("/")
 def root():
     return {"message": "ScoopJoy Backend Running 🚀"}
 
-# TEST API
+# TEST
 @app.get("/api/")
 def home():
     return {"message": "Hello World"}
